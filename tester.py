ce8d2fa3681be37c31dac3ab9e2983073489c71c
Muhammed Sural <muhammedsural@gmail.com>
2022-01-16 20:23:07 +0300

    002

--- tester.py
+++ tester.py
@@ -1,5 +1,5 @@
 import malzememodelleri as md
-f_ywd = 420
+"""f_ywd = 420
 f_co = 30 #MPa
 b=300
 h=500
@@ -13,4 +13,10 @@ gövde_donatı_adeti =1
 x_koladeti = 2
 y_koladeti = 3
 x,y =md.tbdy_mander("S420",f_co,b,h,s,A_s,etriye_çapı,boyuna_donatı_çapı,pas_payı,baslık_donatı_adeti,gövde_donatı_adeti,x_koladeti,y_koladeti,annotate=1)
-eps,fs = md.celik_modeli("S420")
+eps,fs = md.celik_modeli("S420")"""
+
+import section as sc
+import material as mat
+
+steel = mat.define_material.steel("S220")
+print(steel)
