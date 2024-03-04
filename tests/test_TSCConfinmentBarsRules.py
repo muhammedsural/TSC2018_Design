e8056ab6fcd0a36769bc216240f96856d189b00e
Muhammed Sural <59420199+muhammedsural@users.noreply.github.com>
2024-03-04 13:15:18 +0300

    duzenlemeler

---
+++ tests/test_TSCConfinmentBarsRules.py
@@ -0,0 +1,26 @@
+from TSCConfimentBarsRules import ConfimentDesign
+
+"""Units N,mm"""
+Nd                      = 16000 
+B                       = 400
+H                       = 400
+s                       = 80
+TieRebarDiameter        = 8
+LongnitRebarDiameter    = 14
+ClearCoverConc          = 25
+NumBarsTop              = 2
+NumBarsInterior         = 1
+NumBarsBot              = 2
+X_tiebars               = 2
+Y_tiebars               = 3
+fsy                     = 220
+fywe                    = 220
+eps_su                  = 0.08
+f_co                    = 25
+f_ce                    = 25
+Fctd                    = 10
+Ln                      = 2600
+
+@pytest.fixture(scope="class")
+def setUpClass():
+    Confinment = ConfimentDesign(Nd, fsy, Fctd, Ln, B, H, ClearCoverConc, X_tiebars, Y_tiebars, f_co, fywe, TieRebarDiameter, LongnitRebarDiameter)
